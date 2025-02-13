4d085f00cc93eba80c0cd28eba4b5f3206d16542
Clemens <->
2025-02-13 21:20:13 +0100

    fixed helper script docker cp path

--- scripts/set_config_php.sh
+++ scripts/set_config_php.sh
@@ -1,8 +1,6 @@
 SOURCE_DIR=./../nextcloud/config
 CONTAINER=nextcloud_custom
 
-mkdir -p $SOURCE_DIR
-
-docker cp ./nextcloud/config/config.php $CONTAINER:/var/www/html/nextcloud/config/config.php
+docker cp $SOURCE_DIR/config.php $CONTAINER:/var/www/html/nextcloud/config/config.php
 
 docker exec -u root $CONTAINER chown www-data:www-data /var/www/html/nextcloud/config/config.php
