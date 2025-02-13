2c4cbe35443f33c0b125f4993c4d8461d666eb49
Clemens <->
2025-02-13 21:12:17 +0100

    added helper scripts to get/set config.php

---
+++ scripts/get_config_php.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+TARGET_DIR=./nextcloud/config
+
+mkdir -p $TARGET_DIR
+
+docker cp nextcloud_custom:/var/www/html/nextcloud/config/config.php $TARGET_DIR
---
+++ scripts/set_config_php.sh
@@ -0,0 +1,8 @@
+SOURCE_DIR=./nextcloud/config
+CONTAINER=nextcloud_custom
+
+mkdir -p $SOURCE_DIR
+
+docker cp ./nextcloud/config/config.php $CONTAINER:/var/www/html/nextcloud/config/config.php
+
+docker exec -u root $CONTAINER chown www-data:www-data /var/www/html/nextcloud/config/config.php
