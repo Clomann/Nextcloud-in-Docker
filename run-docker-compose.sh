4161ba77a33a58ac8e8c74d9fc7e3858ba13b718
Clemens <->
2025-02-13 21:00:00 +0100

    - moved environment variables into .env file - changed startup script extension to .sh

---
+++ run-docker-compose.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ENV_FILE=conf.env
+
+cd "$(dirname "$0")"
+
+# Ensure conf.env exists
+if [[ ! -f "$ENV_FILE" ]]; then
+    echo "Error: $ENV_FILE not found!"
+    exit 1
+fi
+
+# Load environment variables
+set -a  # Automatically export all variables
+source "$ENV_FILE"
+set +a  # Stop automatically exporting
+
+docker-compose -f docker-compose.yaml up -d
