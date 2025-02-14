3c90c6e103541604c48e79cca3f460fe4ea59787
Clemens <->
2025-02-14 21:26:17 +0100

    added recursive flag to chown

--- scripts/set_certs.sh
+++ scripts/set_certs.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
+FILE=
 TARGET_DIR=/etc/ssl/certs/
 SOURCE_DIR=/etc/letsencrypt/live/
 CONTAINER=nginx_proxy
 
 docker cp -r $SOURCE_DIR/$FILE $CONTAINER:$SOURCE_DIR/$FILE
 
-docker exec -u root $CONTAINER chown www-data:www-data $SOURCE_DIR/$FILE
+docker exec -u root $CONTAINER chown -R www-data:www-data $SOURCE_DIR/$FILE
